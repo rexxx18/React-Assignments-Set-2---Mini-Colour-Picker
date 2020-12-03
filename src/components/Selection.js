202e12feb3fd94cac5a645be081cadc5d24a9efb
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-12-03 21:40:54 +0530

    changes

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,24 @@
+import React, { useState } from "react";
+import "../styles/Child.css";
+const Selection = ({ applyColor }) => {
+  let [backGround, setBackground] = useState({ background: "" });
+  const change = (prop) => {
+    setBackground(prop);
+  };
+  return (
+    <div
+      className="fix-box"
+      onClick={() => applyColor(change)}
+      style={backGround}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+export default Selection;
 
+// When the component is clicked, this state variable should get the value of the nextBackground state variable of the App component. (Hint: use applyColor for this)
+
+// The div component should have a child component that's a h2 tag having text "Selection" and class subheading.
+
+// Export the Selection component by default.
